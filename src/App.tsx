2c6b94ba79b461431156279f6f691aba69aa5e71
Tran Long Tuan <longtuan03@gmail.com>
2024-05-27 11:21:19 +0700

    Add prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes'
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
-
-
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,14 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
-    return (
-        <div className="bg-zinc-900 text-white flex flex-col h-screen">
-          {/* <MainNavbar /> */}
-          <div>PLACE FOR THE NAV BAR</div>
-          <Outlet />
-        </div>
-      );
-}
+  return (
+    <div className="bg-zinc-900 text-white flex flex-col h-screen">
+      {/* <MainNavbar /> */}
+      <div>PLACE FOR THE NAV BAR</div>
+      <Outlet />
+    </div>
+  );
+};
 
-export default MainLayout
-
+export default MainLayout;
